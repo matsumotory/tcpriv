03228ef4826bc037816f9628de903c3a4c615f34
matsumotory <matsu1229@gmail.com>
2020-03-11 19:02:08 +0900

    Implement base structure for netfiler

--- src/tcpriv_module.c
+++ src/tcpriv_module.c
@@ -1,19 +1,44 @@
 #include <linux/module.h>
+#include <linux/init.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
 
 MODULE_AUTHOR("matsumotory");
-MODULE_DESCRIPTION("run kthread");
+MODULE_DESCRIPTION("tcpriv separate privilege on TCP using Linux owner information");
 MODULE_LICENSE("GPL");
 MODULE_INFO(free_form_info, "separate privilege on TCP using task_struct");
 
+/* very useful information: https://kel.bz/post/netfilter */
+
+#define TCPRIV_INFO "tcpriv[info]: "
+
+static struct nf_hook_ops nfho;
+
 static int __init tcpriv_init(void)
 {
-  printk(KERN_INFO "open\n");
+  printk(KERN_INFO TCPRIV_INFO "open\n");
+
+  nfho.hook = hook_func;
+  nfho.hooknum = NF_INET_PRE_ROUTING;
+  nfho.pf = PF_INET;
+  nfho.priority = NF_IP_PRI_FIRST;
+
+  nf_register_hook(&nfho);
+
   return 0;
+}
+
+unsigned int hook_func(unsigned int hooknum, struct sk_buff **skb, const struct net_device *in,
+                       const struct net_device *out, int (*okfn)(struct sk_buff *))
+{
+  printk(KERN_INFO TCPRIV_INFO "Packet!\n");
+  return NF_ACCEPT;
 }
 
 static void __exit tcpriv_exit(void)
 {
-  printk(KERN_INFO "close\n");
+  nf_unregister_hook(&nfho);
+  printk(KERN_INFO TCPRIV_INFO "close\n");
 }
 
 module_init(tcpriv_init);
