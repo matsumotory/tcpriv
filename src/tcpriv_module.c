fe94e3b5fe1adcbea364ed4eae0fc06a062edf77
matsumotory <matsu1229@gmail.com>
2020-03-11 06:58:49 +0000

    Apply clang-format (version 6.0 or later)

--- src/tcpriv_module.c
+++ src/tcpriv_module.c
@@ -5,16 +5,16 @@ MODULE_DESCRIPTION("run kthread");
 MODULE_LICENSE("GPL");
 MODULE_INFO(free_form_info, "separate privilege on TCP using task_struct");
 
-static int __init tcpriv_init(void)    
+static int __init tcpriv_init(void)
 {
   printk(KERN_INFO "open\n");
   return 0;
 }
- 
-static void __exit tcpriv_exit(void)    
+
+static void __exit tcpriv_exit(void)
 {
   printk(KERN_INFO "close\n");
 }
- 
+
 module_init(tcpriv_init);
-module_exit(tcpriv_exit); 
+module_exit(tcpriv_exit);
