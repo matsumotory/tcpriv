5514f15df54c0f961066308c9e412d3c444ea699
matsumotory <matsu1229@gmail.com>
2020-03-11 15:11:00 +0900

    Setup kernel module enviroment

--- misc/provision.sh
+++ misc/provision.sh
@@ -5,12 +5,19 @@ KSHORTVERSION=`uname -r | awk -F. '{printf "%d.%d.%d", $1, $2, $3}'`
 KERNEL_BUILD_HOST=matsumotory
 KERNEL_BUILD_USER=matsumotory
 KERNEL_LOCAL_VER=0.0.1
+SRC_DIR=~/src
+BUILD_DIR=$SRC_DIR/build
+BUILD_MODULE_DIR=$BUILD_DIR/kernel_module
 
 # use ccache
 HOSTCXX=g++
 CC=gcc
 THREAD=2
 
+# download tcpriv
+git clone git@github.com:matsumotory/tcpriv $SRC_DIR
+
+# setup build enviroment
 sudo apt-get update
 sudo apt-get -y install build-essential rake bison git gperf automake m4 \
                 autoconf libtool cmake pkg-config libcunit1-dev ragel \
@@ -22,16 +29,26 @@ sudo apt-get install linux-source-$KSHORTVERSION
 
 sudo update-alternatives --install /usr/bin/clang-format clang-format /usr/bin/clang-format-6.0 1000
 
-if [ -d ./build ]; then
-        rm -rf ./build
+# setup kernel build
+if [ -d $BUILD_DIR ]; then
+  rm -rf $BUILD_DIR
 fi
 
-mkdir ./build
-cd ./build
+mkdir $BUILD_DIR
+cd $BUILD_DIR
 tar xf /usr/src/linux-source-$KSHORTVERSION.tar.bz2
 cd ./linux-source-$KSHORTVERSION
 cp /boot/config-$KVERSION .config
 make olddefconfig
 KBUILD_BUILD_HOST=$KERNEL_BUILD_HOST KBUILD_BUILD_USER=$KERNEL_BUILD_USER USE_CCACHE=1 CCACHE_DIR=~/.ccache make -j$THREAD HOSTCXX="$HOSTCXX" CC="$CC"
 
+# build kernel modules
+if [ -d $BUILD_MODULE_DIR ]; then
+  rm -rf $BUILD_MODULE_DIR
+fi
+
+cd $BUILD_MODULE_DIR
+cp -p $SRC_DIR/Makefile .&& cp -p $SRC_DIR/tcpriv_module.c .
+
+make
 
