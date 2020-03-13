0adcfeea1490b08a549d7ee49c5af75a28505b4a
matsumotory <matsu1229@gmail.com>
2020-03-13 17:40:20 +0900

    When remote host sent a syn packet, remote host is saddr in IP headers

--- src/tcpriv_module.c
+++ src/tcpriv_module.c
@@ -26,7 +26,7 @@ static unsigned int hook_local_in_func(void *priv, struct sk_buff *skb, const st
 
   if (iphdr->version == 4) {
     if (iphdr->protocol == IPPROTO_TCP && tcphdr->syn) {
-      printk(KERN_INFO TCPRIV_INFO "tcpriv found local in TCP syn packet from %pI4.\n", &iphdr->daddr);
+      printk(KERN_INFO TCPRIV_INFO "tcpriv found local in TCP syn packet from %pI4.\n", &iphdr->saddr);
     }
   }
 
