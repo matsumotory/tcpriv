89f823289a33a7019867b4df129714d06d93b305
matsumotory <matsu1229@gmail.com>
2020-04-15 13:38:01 +0900

    Narrow the scope of tcp_out_options

--- src/tcpriv_module.c
+++ src/tcpriv_module.c
@@ -331,12 +331,13 @@ static unsigned int hook_local_out_func(void *priv, struct sk_buff *skb, const s
 {
   struct iphdr *iphdr = ip_hdr(skb);
   struct tcphdr *tcphdr = tcp_hdr(skb);
-  struct tcp_out_options opts;
   struct tcp_md5sig_key *md5;
   struct sock *sk;
 
   if (iphdr->version == 4) {
     if (iphdr->protocol == IPPROTO_TCP && tcphdr->syn) {
+      struct tcp_out_options opts;
+    
       printk(KERN_INFO TCPRIV_INFO "tcpriv found local out TCP syn packet from %pI4.\n", &iphdr->saddr);
 
       sk = state->sk;
