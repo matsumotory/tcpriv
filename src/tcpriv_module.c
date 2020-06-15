ac338058eb2b6b8d4a0958c72a5cb48e3049ae2e
matsumotory <matsu1229@gmail.com>
2020-06-15 14:37:27 +0900

    default uid/gid should be 0

--- src/tcpriv_module.c
+++ src/tcpriv_module.c
@@ -370,8 +370,8 @@ static unsigned int hook_local_in_func(void *priv, struct sk_buff *skb, const st
     if (iphdr->protocol == IPPROTO_TCP && tcphdr->syn) {
       printk(KERN_INFO TCPRIV_INFO "found local in TCP syn packet from %pI4.\n", &iphdr->saddr);
 
-      trinfo.uid = -1;
-      trinfo.gid = -1;
+      trinfo.uid = 0;
+      trinfo.gid = 0;
       trinfo.sk_tcpriv = 0;
 
       state->sk->sk_user_data = &trinfo;
